dd25888782091f7ee38e8b41404f702aeca4f71a
Bianca Kendall <[D)>
2022-05-17 22:23:19 +0200

    added SHOW requests

--- src/main/java/com/bnta/chocolate/controllers/EstateController.java
+++ src/main/java/com/bnta/chocolate/controllers/EstateController.java
@@ -7,10 +7,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping(name="estates")
@@ -19,9 +21,16 @@ public class EstateController {
     @Autowired
     EstateRepository estateRepository;
 
+    //GET
     @GetMapping
     public ResponseEntity<List<Estate>> getEstates() {
         return new ResponseEntity<>(estateRepository.findAll(), HttpStatus.OK);
+    }
+
+    //SHOW
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<Optional<Estate>> getEstate(@PathVariable Long id) {
+        return new ResponseEntity(estateRepository.findById(id), HttpStatus.OK);
     }
 
 }
