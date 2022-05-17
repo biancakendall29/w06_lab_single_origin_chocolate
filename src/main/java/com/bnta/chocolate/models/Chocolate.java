778ba59aafa59e21560b397aa1fb6d0a1a418794
Bianca Kendall <[D)>
2022-05-17 20:58:32 +0200

    added annotations to chocolates and estates

--- src/main/java/com/bnta/chocolate/models/Chocolate.java
+++ src/main/java/com/bnta/chocolate/models/Chocolate.java
@@ -2,11 +2,19 @@ package com.bnta.chocolate.models;
 
 import javax.persistence.*;
 
-
+@Entity
+@Table(name="chocolates")
 public class Chocolate {
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    @Column
     private Long id;
+    @Column
     private String name;
+    @Column
     private int cocoaPercentage;
+    @ManyToOne
+    @JoinColumn
     private Estate estate;
 
     public Chocolate(String name, int cocoaPercentage, Estate estate) {
